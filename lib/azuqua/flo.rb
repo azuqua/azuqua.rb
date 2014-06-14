94f8bdb61e7497960d7a9ad0324c429035723552
Alec Embke <aembke@gmail.com>
2014-06-14 14:31:03 -0700

    updated with security changes

--- lib/azuqua/flo.rb
+++ lib/azuqua/flo.rb
@@ -1,6 +1,7 @@
 
 require "rubygems"
 require "json"
+require "time"
 require "openssl"
 require "net/https"
 require "uri"
@@ -10,11 +11,11 @@ class Flo
 	ROUTES = {
 		:invoke => {
 			:path => "/api/flo/:id/invoke",
-			:method => "POST"
+			:method => "post"
 		},
 		:list => {
 			:path => "/api/account/flos",
-			:method => "POST"
+			:method => "get"
 		}
 	}
 
@@ -29,24 +30,25 @@ class Flo
 
 	attr_reader :name, :alias
 
-	def initialize name, _alias
-		@name = name
+	def initialize _name, _alias
+		@name = _name
 		@alias = _alias
 	end
 
 	# static method to list all flos for your account
-	def self.list refresh=false
-		if refresh || @@floCache.nil?
-			@@floCache = self.request(ROUTES[:list][:path], {}).map { |f| self.new(f["name"], f["alias"]) }
+	def self.all _refresh=false
+		if _refresh || @@floCache.nil?
+			@@floCache = self.request(ROUTES[:list][:path], ROUTES[:list][:method], nil)
+				.map { |f| self.new(f["name"], f["alias"]) }
 		else
 			@@floCache
 		end
 	end
 
 	# instance method to invoke a flo
-	def invoke data
+	def invoke _data
 		if @name && @alias
-			self.class.request(ROUTES[:invoke][:path].gsub(":id", @alias), data)["data"]
+			self.class.request(ROUTES[:invoke][:path].gsub(":id", @alias), ROUTES[:invoke][:method], _data)
 		else
 			raise "Invalid flo name or alias"
 		end
@@ -54,17 +56,33 @@ class Flo
 
 	private
 
-	def self.request path, data
-		body = {
-			:accessKey => self.account[:accessKey],
-			:data => data,
-			:hash => self.sign_data(self.account[:accessSecret], data)
-		}
-		uri = URI.parse(HTTP_OPTIONS[:host] + path)
+	def add_get_parameter _path, _key, _value
+		delimiter = _path.include?("?") ? "&" : "?"
+		_path + delimiter + URI.encode_www_form_component(_key) + "=" + URI.encode_www_form_component(_value)
+	end
+
+	def self.request _path, _verb, _data
+		_data = "" if _data.nil? || _data.empty?
+		headers = HTTP_OPTIONS[:headers]
+		timestamp = Time.now.utc.iso8601
+		headers["x-api-timestamp"] = timestamp
+		headers["x-api-accessKey"] = self.account[:accessKey]
+		headers["x-api-hash"] = self.sign_data(self.account[:accessSecret], _data, _verb, _path, timestamp)
+		if _verb == "get" && _data.is_a?(Hash)
+			_path = _data.inject(_path) { |s, c| add_get_parameter(s, c[0], c[1]) }
+		elsif _verb == "post"
+			body = _data.to_json
+			headers["Content-Length"] = body.bytesize.to_s
+		end
+		uri = URI.parse(HTTP_OPTIONS[:host] + _path)
 		https = Net::HTTP.new(uri.host, uri.port)
 		https.use_ssl = true
-		req = Net::HTTP::Post.new(uri.path, HTTP_OPTIONS[:headers])
-		req.body = body.to_json
+		if _verb == "get"
+			req = Net::HTTP::Get.new(uri.path, headers)
+		else
+			req = Net::HTTP::Post.new(uri.path, headers)
+			req.body = body
+		end
 		res = https.request(req)
 		code = res.code.to_i
 		case code
@@ -77,8 +95,10 @@ class Flo
 		::Azuqua.account
 	end
 
-	def self.sign_data secret, data
-		OpenSSL::HMAC.hexdigest("sha256", secret, data.to_json)
+	def self.sign_data _secret, _data, _verb, _path, _timestamp
+		_data = _data.to_json if _data.is_a?(Hash)
+		meta = [_verb.downcase, _path, _timestamp].join(":")
+		OpenSSL::HMAC.hexdigest("sha256", _secret, meta + _data)
 	end
 
 end
